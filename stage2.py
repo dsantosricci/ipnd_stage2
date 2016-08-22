848b4ec9f6a91032b801593d9adffcfdbc3a36cc
Daniel Ricci <dsantosricci@gmail.com>
2016-08-22 01:07:05 -0300

    Update stage2.py

--- stage2.py
+++ stage2.py
@@ -1,8 +1,14 @@
 # IPND Stage 2 Final Project
+# This piece of code was made to pass the stage2 grading. It asks for guesses as user input and it gives if the guesses matches with the blank space in the text.
 
+levels = ["easy", "medium", "hard"]
+# asks user to select a level
 difficulty = raw_input('Please select a game difficulty by typing it in!\nPossible choices include easy, medium, and hard.   \n\n')
-print "\n\nYou have chosen " + difficulty + "  !" 
-print "\nDepending on your answer to the previous question, you are going to read about data mining, machine learning or deep learning.\nGood luck!\n"
+while difficulty not in levels:
+    print "\nSomething is wrong here. You have entered a wrong answer! Please input a valid value.\n"
+    difficulty = raw_input('Please select a game difficulty by typing it in!\nPossible choices include easy, medium, and hard.   \n\n')
+
+print "You have chosen " + difficulty + "  !\nYou are going to read about data mining, machine learning and deep learning.\nGood luck!\n"
 
 easy = "Data __1__ can be considered a superset of many different methods to extract __2__ from data. It might involve traditional statistical methods and machine learning. Data mining applies methods from many different areas to identify previously unknown __3__ from data. This can include statistical __4__, machine learning, text analytics, time series analysis and other areas of analytics. Data mining also includes the study and practice of data storage and data __5__."
 easy_answer = ["mining", "insights",  "patterns",  "algorithms",  "manipulation"]
@@ -16,43 +22,46 @@ hard_answer = ["neural", "objects", "looking", "recognition",  "translation"]
 sub = ["__1__", "__2__", "__3__", "__4__", "__5__"]
 
 
-#this procedure helps us to avoid repetition in our code
+# this procedure takes the user input - the difficulty level he/she intends to play the game - and returns the text (level_string) and the answer (answer_string) associated with that level.
 def selectdif(difficulty):
     if difficulty == 'easy':
         print easy
         return easy, easy_answer
-    if difficulty == 'medium':
+    elif difficulty == 'medium':
         print medium
         return medium, medium_answer
-    if difficulty == 'hard':
+    elif difficulty == 'hard':
         print hard
         return hard, hard_answer
     
+
 level_string, answer_string = selectdif(difficulty)
+
   
-#this procedure check if the expression is correct
+# this procedure checks if the guess input given by the user is correct. It takes the level_string and the answer_string and verifies with the input matches with some of the possible answers. If incorrect, the procedure informs the user.
 def play_game(level_string, answer_string):
+    num_answers = 5
     guesses = raw_input('\nplease inform how many guesses do you want to have    \n')
     print "\nYou will get " + guesses + " guesses per problem"
-    i = 0 #index
-    c = 0 #count
-    d = int(guesses)
+    i = 0 # index
+    count = 0 
+    guesses = int(guesses)
 
-    while(i<=5 or c <= d):
+    while(i<=num_answers or count <= guess):
         guess = raw_input ("\nType in a guess for blank  \n" + str(i+1) )
         if guess==answer_string[i]:
            level_string = level_string.replace(sub[i], answer_string[i])
            print "\nGreat job, This is the correct answer.\n"
            print level_string
-           i = i + 1
+           i += 1
         else:
             print "\n\nThis is not the correct answer, please try again."
-            c = c + 1
-        if(c>d or i==5):
+            count = count + 1
+        if(count>=guesses or i==num_answers):
             break
-    if (c >= d):
+    if (count == guesses):
         print "\nsorry, game over."
-    if (i >= 5):
+    elif (i >= num_answers):
         print "\nCongratulations, you have successfully answered the quiz."
      
         
